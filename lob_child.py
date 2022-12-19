429eebc5eae25587abee563e709cc4a336714c65
Divya Rams <92635226+Divyarams@users.noreply.github.com>
2022-12-19 11:58:02 +0530

    Create lob_child.py

---
+++ lob_child.py
@@ -0,0 +1,26 @@
+from common_parent import main
+class lob_child(common_parent):
+  
+  def __init__(self):
+    # call super function to read parent rule file methods and attributes
+    super().__init__()
+    print('Parent class instantiated')
+    
+  def lob_rule1():
+    global var1
+    array_check=['CA','NY','LA']
+    var1=df.filter(col('COL_NAME')).isin(array_check)).count()
+    return var1
+    
+  def lob_rule2():
+    global var2
+    array_checksum=1000000
+    if df.select(sum(col('COL_NAME'))) > array_checksum:
+      var2=1
+    else:
+      var2=0
+    return var2
+   
+rule_check=lob_child()
+rule_check.check_null()
+rule_check.check_notnull()
